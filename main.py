28a2aa6c0c7750d94c48915aa49f40ce9ceed2d2
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-06-06 21:42:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import functions
 
 
 while True:
-    print('1. вывод, 2. добавление, 3. поиск')
+    print('1. вывод, 2. добавление, 3. поиск, 4. изменение, 5. удаление')
     mode = int(input())
     if mode == 1:
         functions.show_data()
@@ -10,5 +10,9 @@ while True:
         functions.add_data()
     elif mode == 3:
         functions.find_data()
+    elif mode == 4:
+        functions.edit_data()
+    elif mode == 5:
+        functions.delete_data()
     else:
         break
