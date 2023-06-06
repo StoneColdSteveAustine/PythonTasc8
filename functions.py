1da77a9c865cdcc76a6f3ae93390abad6bef1747
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-06-06 21:43:08 +0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -65,34 +65,3 @@ print(f"Удалена запись: {del_data_lines}\n")
 with open(book.txt, «w», encoding=’utf-8′) as data:
 data.write("\n".join(data_lines))
 
-def main():
-my_choice = -1
-file_tel = «tel.txt»
-
-# Создает файл если его нет в папке
-with open(file_tel, «a», encoding=»utf-8″) as file:
-file.write(«»)
-
-while my_choice != 0:
-print(«Выберите одно из действий:»)
-print(«1 — Вывести инфо на экран»)
-print(«2 — Произвести экпорт данных»)
-print(«3 — Произвести изменение данных»)
-print(«4 — Произвести удаление данных»)
-print(«0 — Выход из программы»)
-action = int(input(«Действие: «))
-if action == 1:
-show_data(file_tel)
-elif action == 2:
-export_data(file_tel)
-elif action == 3:
-edit_data(file_tel)
-elif action == 4:
-delete_data(file_tel)
-else:
-my_choice = 0
-
-print(«До свидания»)
-
-if __name__ == «__main__»:
-main()
